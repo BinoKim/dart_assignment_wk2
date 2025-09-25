19aecb4f7d7fde023708ed7ec3f34129225988a8
BinoKim <euibin9738@gmail.com>
2025-09-25 15:42:52 +0900

    1번 문제 주석 업데이트

--- lib/1st_if.dart
+++ lib/1st_if.dart
@@ -1,8 +1,7 @@
 // 1번 문제
 // if, else if, else로 풀기
 
-// int testResult = 84;  
-
+// if 조건문을 통해 점수를 비교하고 참일 경우 해당 동작 실행
 void score (int testResult) {
   if (testResult >= 90) {
     print("이 학생의 점수는 $testResult점 이며, 등급은 A 등급입니다!");
@@ -15,6 +14,7 @@ void score (int testResult) {
 
 void main() {
   
+  // 함수와 해당 파라미터의 숫자로 동작 실행
   score(79);
   score(97);
   score(84);
